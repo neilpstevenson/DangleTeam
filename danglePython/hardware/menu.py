c16d7f7191b56c15abec65023a48801afaa49bc3
--global <neil.git@secureness.co.uk>
2020-01-23 11:00:36 +0000

    Add exe support

--- danglePython/hardware/menu.py
+++ danglePython/hardware/menu.py
@@ -9,44 +9,39 @@ from luma.core.render import canvas
 
 class MenuDisplay:
 	def __init__(self, title, menuItems, selected):
-		self.titleFont = self.make_font("code2000.ttf", 14)
-		self.unselectedFont = self.make_font("code2000.ttf", 8)
-		self.selectedFont = self.make_font("code2000.ttf", 20)
+		self.titleFont = ImageFont.truetype("Piboto-Bold.ttf", 14)
+		self.unselectedFont = ImageFont.truetype("Piboto-Regular.ttf", 10)
+		self.selectedFont = ImageFont.truetype("Piboto-Bold.ttf", 20)
 		self.title = title
 		self.menuItems = menuItems
 		self.selected = selected
 		self.device = get_device()
 	
-	def make_font(self, name, size):
-		font_path = os.path.abspath(os.path.join(
-			os.path.dirname(__file__), 'fonts', name))
-		return ImageFont.truetype(font_path, size)
-
 	def show(self):
-		top = -2
+		top = 0
 		with canvas(self.device) as draw:
 			# Title - inverse text centered
 			w, h = draw.textsize(text=self.title, font=self.titleFont)
 			left = (self.device.width - w) / 2
 			draw.rectangle(((0, top), (self.device.width, h)), fill="white")
-			draw.text((left, top), text=self.title, font=self.titleFont, fill="black")
-			top += h
+			draw.text((left, top-2), text=self.title, font=self.titleFont, fill="black", spacing=-2)
+			top += h-2
 			# Unselected initial item
 			if self.selected > 0:
 				item = self.selected - 1
 				w, h = draw.textsize(text=self.
 				menuItems[item], font=self.unselectedFont)
-				draw.text((10, top), text=self.menuItems[item], font=self.unselectedFont, fill="white")
-				top += h
+				draw.text((10, top), text=self.menuItems[item], font=self.unselectedFont, fill="white", spacing=-2)
+				top += h-4
 			# Then the selected item
 			w, h = draw.textsize(text=self.menuItems[self.selected], font=self.selectedFont)
-			draw.text((0, top), text=self.menuItems[self.selected], font=self.selectedFont, fill="white")
+			draw.text((0, top), text=self.menuItems[self.selected], font=self.selectedFont, fill="white", spacing=-2)
 			top += h
 			# Then the rest of the selected items while we have room
 			for item in range(self.selected+1, len(self.menuItems)):
 				if top < self.device.height:
 					w, h = draw.textsize(text=self.menuItems[item], font=self.unselectedFont)
-					draw.text((10, top), text=self.menuItems[item], font=self.unselectedFont, fill="white")
+					draw.text((10, top), text=self.menuItems[item], font=self.unselectedFont, fill="white", spacing=-2)
 					top += h
 
 	def select(self, selected):
