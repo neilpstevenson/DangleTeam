31b30ef380e4d5de84ccbe4a598ed0a75a235676
Neil Stevenson <neil.git@secureness.co.uk>
2024-02-07 22:08:32 +0000

    Change Menu to read JSON file

--- danglePython/MenuStarterPyGame.py
+++ danglePython/MenuStarterPyGame.py
@@ -2,6 +2,7 @@ import time
 import numpy as np
 import psutil
 import sys
+import json
 # Interfaces
 from interfaces.SensorAccessFactory import SensorAccessFactory
 from analysis.StepUpDownButtonValue import StepUpDownButtonValue
@@ -18,9 +19,12 @@ def Run(selected, actionParams):
 	# Already running?
 	alreadyRunning = False
 	for process in psutil.process_iter():
-		if process.cmdline() == actionParams:
-			alreadyRunning = True
-			break
+		try:
+			if process.cmdline() == actionParams:
+				alreadyRunning = True
+				break
+		except psutil.NoSuchProcess:
+			pass
 			
 	pythonName = actionParams[1]
 	if not alreadyRunning:
@@ -39,12 +43,15 @@ def StopAll(selected, actionParams):
 		for processCmd in options:
 			print(f"Checking: {processCmd}")
 			for process in psutil.process_iter():
-				if processCmd[0] == Run and process.cmdline() == processCmd[1]:
-					pythonName = processCmd[1][1]
-					print(f"Stopping: {processCmd}")
-					display.message(f"Stopping\n{pythonName}")
-					process.kill()
-					time.sleep(0.2)
+				try:
+					if processCmd[0] == Run and process.cmdline() == processCmd[1]:
+						pythonName = processCmd[1][1]
+						print(f"Stopping: {processCmd}")
+						display.message(f"Stopping\n{pythonName}")
+						process.kill()
+						time.sleep(0.2)
+				except psutil.NoSuchProcess:
+					pass
 
 def DisplayScreenSaver(selected, actionParams):
 	stopButton = sensors.button(10)
@@ -98,35 +105,67 @@ def Quit(selected, actionParams):
 
 sensors = SensorAccessFactory.getSingleton()
 title = "Select Challenge"
-menus = np.array([
-	["Manual Control", [ 
-			[Run, ['python3','DangleRun.py', '--challenge', 'ChallengeManualSequence']],
-			[Run, ['python3','displayStatus.py']],
-			[DisplayPositions, []],
-			[DisplayText, ['Manual Control']]  ]], 
-	["Tidy up the Toys", [ 
-			[Run, ['python3','TidyUpTheToysImageProcess.py']], 
-			[Run, ['python3','DangleRun.py', '--challenge', 'ChallengeTidyTheToys']],
-			[Run, ['python3','displayStatus.py']],
-			[DisplayText, ['Tidy up the\nToys']] ]], 
-	["Up the\nGarden Path", [
-			[Run, ['python3','VisionLineProcessor.py']], 
-			[Run, ['python3','VoiceRecogniser.py']], 
-			[Run, ['python3','DangleRun.py', '--challenge', 'ChallengeUpTheGardenPath']],
-			[Run, ['python3','displayStatus.py']],
-			[DisplayText, ['Up the Garden\nPath']] ]], 
-	["Feed the Fish", [
-			[Run, ['python3','FeedFishImageProcess.py']], 
-			[Run, ['python3','DangleRun.py', '--challenge', 'ChallengeFeedTheFish']],
-			[Run, ['python3','displayStatus.py']],
-			[DisplayText, ['Feed the Fish']] ]],
-	["Stop all", [
-			[StopAll, []] ]],
-	["Quit", [
-			[Quit, []] ]], 
-	["Welcome", [
-			[DisplayScreenSaver, []] ]]
-	])
+menus = np.empty(shape=[0, 2], dtype=object)
+#np.array([
+#
+#	["Manual Control", [ 
+#			[Run, ['python3','DangleRun.py', '--challenge', 'ChallengeManualSequence']],
+#			[Run, ['python3','displayStatus.py']],
+#			[DisplayPositions, []],
+#			[DisplayText, ['Manual Control']]  ]], 
+#	["Tidy up the Toys", [ 
+#			[Run, ['python3','TidyUpTheToysImageProcess.py']], 
+#			[Run, ['python3','DangleRun.py', '--challenge', 'ChallengeTidyTheToys']],
+#			[Run, ['python3','displayStatus.py']],
+#			[DisplayText, ['Tidy up the\nToys']] ]], 
+#	["Up the\nGarden Path", [
+#			[Run, ['python3','VisionLineProcessor.py']], 
+#			[Run, ['python3','VoiceRecogniser.py']], 
+#			[Run, ['python3','DangleRun.py', '--challenge', 'ChallengeUpTheGardenPath']],
+#			[Run, ['python3','displayStatus.py']],
+#			[DisplayText, ['Up the Garden\nPath']] ]], 
+#	["Feed the Fish", [
+#			[Run, ['python3','FeedFishImageProcess.py']], 
+#			[Run, ['python3','DangleRun.py', '--challenge', 'ChallengeFeedTheFish']],
+#			[Run, ['python3','displayStatus.py']],
+#			[DisplayText, ['Feed the Fish']] ]],
+#	["Stop all", [
+#			[StopAll, []] ]],
+#	["Quit", [
+#			[Quit, []] ]], 
+#	["Welcome", [
+#			[DisplayScreenSaver, []] ]]
+#	], 
+#	dtype=object)	# Needed in later numpy versions, to prevent "inhomogenous" errors
+
+# Load the menu definitions
+with open("Menus.json", "r") as read_file:
+	json_menu = json.load(read_file)
+#print(json_menu)
+# Convert to our runtime array
+for menu_item in json_menu:
+	name = menu_item["name"]
+	actions = []
+	for nextaction in menu_item["actions"]:
+		action = nextaction["action"]
+		params = nextaction["params"]
+		if action == 'Run':
+			actions = actions + [[Run, params]]
+		elif action == 'DisplayText':
+			actions = actions + [[DisplayText, params]]
+		elif action == 'DisplayPositions':
+			actions = actions + [[DisplayPositions, params]]
+		elif action == 'StopAll':
+			actions = actions + [[StopAll, params]]
+		elif action == 'Quit':
+			actions = actions + [[Quit, params]]
+		elif action == 'DisplayScreenSaver':
+			actions = actions + [[DisplayScreenSaver, params]]
+		else:
+			raise ValueError(f"Unknown action '{action}'") 
+	nextmen = np.array([[name, actions]], dtype=object)
+	menus = np.append(menus, nextmen, axis=0)
+#print(f"results: {menus}")
 
 # Enable the menu selected via joystick
 display = MenuDisplay(title, menus[:,0], 0, 3)
